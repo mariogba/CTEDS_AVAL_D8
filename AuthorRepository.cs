61ac67388688aa465572eaa06521310de43a6ff8
mariogba <110565636+mariogba@users.noreply.github.com>
2022-11-21 18:50:35 -0300

    Add files via upload

---
+++ AuthorRepository.cs
@@ -0,0 +1,33 @@
+﻿using BooksAPI.Contexts;
+using BooksAPI.Domains;
+using BooksAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Repositories;
+
+public class AuthorRepository : IAuthor
+{
+    private readonly Context ctx;
+
+	public AuthorRepository(Context ctx)
+	{
+		this.ctx = ctx;
+	}
+    public List<Author> List()
+    {
+        return ctx.Authors.Include(d => d.Books).ToList();
+    }
+     public Author Create(Author author)
+    {
+        var val = ctx.Authors.Add(author);
+        ctx.SaveChanges();
+        return val.Entity;
+    }
+
+    void IAuthor.Create(Author Author)
+    {
+        throw new NotImplementedException();
+    }
+
+   
+}
